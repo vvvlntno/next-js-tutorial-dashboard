0c683778329947e676ce38fad4fce5f6258b5363
vvvlntno <vvvlntno@gmail.com>
2025-10-11 11:42:45 +0200

    feat: added secondary font

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -1,3 +1,9 @@
 import { Inter } from 'next/font/google';
+import { VT323 } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
+
+export const vt323 = VT323({ 
+    weight: '400',
+    subsets: ['latin'],
+});
