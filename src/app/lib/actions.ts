c101e06cba7061f01f323bb5722ddba98d72382e
vvvlntno <vvvlntno@gmail.com>
2025-10-15 19:56:25 +0200

    fix(invoices): commented out update invoice error message return

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -63,7 +63,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   } catch (error) {
     // We'll also log the error to the console for now
     console.error(error);
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    // return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
   revalidatePath('/dashboard/invoices');
