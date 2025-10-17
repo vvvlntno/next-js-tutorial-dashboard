0ee8dfaf6fba8f2d8e46853a381d068a979a41b3
valentino <82881487+vvvlntno@users.noreply.github.com>
2025-10-17 12:12:49 +0200

    PR: merge `feature/adding-metadata` into `main` (#16)
    * feat(metadata): moved favicon and og image into app [2dead09](https://github.com/vvvlntno/next-js-tutorial-dashboard/commit/2dead09fb29587f93454f27210bb390e7a191714)
    
    * feat(metadata): added template metadata to every page [6a82ff8](https://github.com/vvvlntno/next-js-tutorial-dashboard/commit/6a82ff8ba68263c8530fd06d8b90cda5614563a5)

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -8,7 +8,12 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 export default async function Page() {
   return (
     <main>
